9708c4cae1d955aac88d7ed6e9dc4de8c54b0fd5
SnakeHacKx <omaralexisxdx2@gmail.com>
2021-01-15 16:22:20 -0500

    Se creó una nueva escena de prueba

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hola");
     }
 
     // Update is called once per frame
